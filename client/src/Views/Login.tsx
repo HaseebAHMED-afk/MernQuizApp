6653243c499eebeb8231b7ac4cc66b24972ee55f
HaseebAHMED-afk <haseeb.ahmed456123@gmail.com>
2021-06-23 23:13:17 +0500

    Login page complete

--- client/src/Views/Login.tsx
+++ client/src/Views/Login.tsx
@@ -1,16 +1,34 @@
-import React from 'react'
+import React ,{useState} from 'react'
 import '../App.css'
+import { TextField } from '@material-ui/core'
 
 const Login = () => {
-    return (
-        <div className='login-page p-5 text-center' >
-            <h1>Login</h1>
-            <section className='p-5' >
-            <div className=" w-50 m-auto input-group mb-3">
-  <input type="text" className="form-control" placeholder="Recipient's username" aria-label="Recipient's username" aria-describedby="basic-addon2" />
-  <span className="input-group-text" id="basic-addon2">@example.com</span>
-</div>
+    let [email , setEmail] = useState<String>('')
+    let [password , setPassword] = useState<String>('')
 
+    const handleLoginSubmit = () =>{
+        console.log({
+            email ,
+            password
+        });
+
+        setEmail('')
+        setPassword('')
+        
+    }
+    return (
+        <div className='login-page p-3 text-center' >
+            <h1 className='mt-5' >Login</h1>
+            <section className=' login-section m-auto p-5' >
+            <div className="input-group input-group-sm mb-3">
+            <TextField variant='outlined' color='primary' label='Email' value={email} onChange={(e)=> setEmail(e.target.value)} fullWidth />
+            </div>
+            <div className="input-group input-group-sm mb-3">
+                <TextField variant='outlined' color='primary' onChange={(e)=> setPassword(e.target.value)} label='Password' value={password} type='password' fullWidth />
+                </div>
+                <button className='btn btn-dark' onClick={handleLoginSubmit} >
+                    Log In
+                </button>
             </section>
         </div>
     )
