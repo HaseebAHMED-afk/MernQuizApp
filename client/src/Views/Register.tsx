6d4475f35a98a7e1767aa7324ef7c60c9f536faa
HaseebAHMED-afk <haseeb.ahmed456123@gmail.com>
2021-06-26 17:58:15 +0500

    Register page complete

--- client/src/Views/Register.tsx
+++ client/src/Views/Register.tsx
@@ -1,9 +1,31 @@
 import React from 'react'
+import { Button, TextField } from '@material-ui/core'
+import '../App.css'
+import GTranslateIcon from '@material-ui/icons/GTranslate'
+import FacebookIcon from '@material-ui/icons/Facebook';
 
 const Register = () => {
     return (
         <div>
-            register page
+            <div className='reg-page p-3 text-center' >
+            <h1 className='mt-5' >Register</h1>
+            <section className=' login-section m-auto p-5' >
+            <div className="input-group input-group-sm mb-3">
+            <TextField variant='outlined' color='primary' label='Email'  fullWidth />
+            </div>
+            <div className="input-group input-group-sm mb-3">
+                <TextField variant='outlined' color='primary' label='Password'  type='password' fullWidth />
+                </div>
+                <button className='btn btn-dark' >
+                    Log In
+                </button>
+            </section>
+        </div>  
+        <hr style={{width:'50%' , margin:'auto'}} />
+        <div className='or-section' >
+            <Button startIcon={<GTranslateIcon />} variant='outlined' color='secondary'  >Sign Up With Google</Button>
+            <Button startIcon={<FacebookIcon />} variant='outlined' color='primary'  >Sign Up With Facebook</Button>
+        </div>
         </div>
     )
 }
