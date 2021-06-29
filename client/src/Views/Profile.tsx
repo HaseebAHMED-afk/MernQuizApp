95e2e7156c9d70d768300ba3f1bb6c0cbba3db11
HaseebAHMED-afk <haseeb.ahmed456123@gmail.com>
2021-06-29 18:15:24 +0500

    Profile complete

--- client/src/Views/Profile.tsx
+++ client/src/Views/Profile.tsx
@@ -3,15 +3,15 @@ import React from 'react'
 const Profile = () => {
     return (
         <div>
-            <section className="bg-dark text-light  text-center p-5">
+            <section className="bg-dark text-light  text-center p-5cd">
             <img src='https://i.pinimg.com/originals/ba/c1/37/bac13770cdea801e72852d8dc7160295.png' className='profile-img' alt='profile-img' />
            
                 <div className="row px-5 w-75 mx-auto" >
-                <div className="text-start col-md-6 col-md-4">
+                <div className="text-center col-sm-6 text-md-start ">
                     <h1>Name</h1>
                     <p>Tag</p>
                 </div>
-                <div className="text-end col-md-6 col-md-4 ">
+                <div className="text-center col-sm-6 text-md-end ">
                     <h1>Level</h1>
                     <p>Score</p>
                 </div>
@@ -27,7 +27,7 @@ const Profile = () => {
                             <div className="card bg-dark text-light">
                                 <div className="card-body text-center">
                                     
-                                    <h1 className="card-title mb-3">Total time taken</h1>
+                                    <h1 className="card-title mb-3">Average time taken</h1>
                                     <p>insert graph here</p>
                                     <p className="card-text">
                                         on a total of {'number'} quizzes
@@ -39,12 +39,10 @@ const Profile = () => {
                         <div className='col-md' >
                             <div className="card bg-primary text-light">
                                 <div className="card-body text-center">
-                                    <div className="h1 mb-3">
-                                        <i className="bi bi-bar-chart-steps"></i>
-                                    </div>
-                                    <h3 className="card-title mb-3">In-depth analysis.</h3>
+                                    <h1 className="card-title mb-3">Success Ratio.</h1>
+                                    <p>insert graph here</p>
                                     <p className="card-text">
-                                        Track your stats, your hi's and lo's.
+                                        for a total of {'number'} quizzess.
                                     </p>
                                 </div>
                             </div>
@@ -54,17 +52,96 @@ const Profile = () => {
                         <div className='col-md' >
                             <div className="card bg-success text-light">
                                 <div className="card-body text-center">
-                                    <div className="h1 mb-3">
-                                        <i className="bi bi-bookmark-star-fill"></i>
-                                    </div>
-                                    <h3 className="card-title mb-3">Specialized rewarding system.</h3>
+                                    <h1 className="card-title mb-3">Standard Deviation</h1>
+                                    <p>insert graph here</p>
                                     <p className="card-text">
-                                        Get custom badges for your achievements.
+                                        something
                                     </p>
                                 </div>
                             </div>
                         </div>
                     </div>
+                </div>
+            </section>
+
+            <section className='p-5' >
+                <div className='container' >
+                    <div className='row text-center g-4' >
+                        <div className='col-md' >
+
+                            <div className="card bg-success text-light">
+                                <div className="card-body text-center">
+                                    
+                                    <h1 className="card-title mb-3">High Score</h1>
+                                    <p>score</p>
+                                    <p className="card-text">
+                                       on quiz
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div className='col-md' >
+                            <div className="card bg-warning text-light">
+                                <div className="card-body text-center">
+                                    <h1 className="card-title mb-3">Average score</h1>
+                                    <p>score</p>
+                                    <p className="card-text">
+                                       on quiz
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+
+
+                        <div className='col-md' >
+                            <div className="card bg-danger text-light">
+                                <div className="card-body text-center">
+                                    <h1 className="card-title mb-3">Lowest Score</h1>
+                                    <p>Score</p>
+                                    <p className="card-text">
+                                       on quiz
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+
+            <section className="p-5">
+                <h1>Most recently attempted</h1>
+                <hr />
+                <div className="row row-cols-1 row-cols-md-3 g-4">
+                    <div className="col">
+                        <div className="card shadow-lg">
+                            <img src="https://yt3.ggpht.com/ytc/AAUvwnjMQg0vNJYMPsEA4X60Oqgj6A_ko4cLP0duHRG0cA=s900-c-k-c0x00ffffff-no-rj"  className="card-img-top" alt="..." />
+                            <div className="card-body">
+                                <h5 className="card-title">Card title</h5>
+                                <p className="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                                <span className="badge bg-danger">Highest Rated</span>
+                            </div>
+                        </div>
+                    </div>
+                    
+                </div>
+            </section>
+
+            <section className="p-5">
+                <h1>Favorite Quizzes</h1>
+                <hr />
+                <div className="row row-cols-1 row-cols-md-3 g-4">
+                    <div className="col">
+                        <div className="card shadow-lg">
+                            <img src="https://yt3.ggpht.com/ytc/AAUvwnjMQg0vNJYMPsEA4X60Oqgj6A_ko4cLP0duHRG0cA=s900-c-k-c0x00ffffff-no-rj"  className="card-img-top" alt="..." />
+                            <div className="card-body">
+                                <h5 className="card-title">Card title</h5>
+                                <p className="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                                <span className="badge bg-danger">Highest Rated</span>
+                            </div>
+                        </div>
+                    </div>
+                    
                 </div>
             </section>
         </div>
