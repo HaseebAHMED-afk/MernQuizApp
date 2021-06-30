542de18d532d3a1203b89658aede86405f5b500e
HaseebAHMED-afk <haseeb.ahmed456123@gmail.com>
2021-07-01 00:57:21 +0500

    Contact us page

--- client/src/Views/ContactUs.tsx
+++ client/src/Views/ContactUs.tsx
@@ -1,10 +1,25 @@
 import React from 'react'
 import '../App.css'
+import {TextField} from '@material-ui/core'
 
 const ContactUs = () => {
     return (
-        <div>
-            <h1>Contact Us</h1>
+        <div className='p-3 text-center' >  
+            <h1 className='mt-5 ' >Contact Us</h1>
+            <section className=' login-section m-auto p-5' >
+            <div className="input-group input-group-sm mb-3">
+            <TextField variant='outlined' color='primary' label='Name' fullWidth />
+            </div>
+            <div className="input-group input-group-sm mb-3">
+            <TextField variant='outlined' color='primary' label='Email' fullWidth />
+            </div>
+            <div className="input-group input-group-sm mb-3">
+                <TextField variant='outlined' color='primary' multiline rows={7}  label='Message'  fullWidth />
+                </div>
+                <button className='btn btn-dark' >
+                    Send
+                </button>
+            </section>
         </div>
     )
 }
