d020260f429212258da272e15c8f5c74845469ea
HaseebAHMED-afk <haseeb.ahmed456123@gmail.com>
2021-07-04 19:12:22 +0500

    config changes

--- server/Config/db.js
+++ server/Config/db.js
@@ -5,7 +5,8 @@ const connectDB = async ()=> {
         const conn = await mongoose.connect('mongodb://localhost:27017/geekQuizDB',{
             useCreateIndex:true,
             useNewUrlParser:true,
-            useUnifiedTopology:true
+            useUnifiedTopology:true,
+            useFindAndModify: false
         });
         console.log('MongoDB connection successful');
     } catch (err) {
